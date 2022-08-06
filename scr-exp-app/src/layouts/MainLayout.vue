8c5e7052c9fae3971a43892f062a3b7a1dd6d355
qs <quirin.schlegel@icloud.com>
2022-08-06 18:26:33 +0200

    00

--- scr-exp-app/src/layouts/MainLayout.vue
+++ scr-exp-app/src/layouts/MainLayout.vue
@@ -1,116 +1,63 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
+  <q-layout view="hHh LpR fFr">
 
-        <q-toolbar-title>
-          Quasar App
+    <q-header elevated class="bg-primary text-black">
+      <q-toolbar>
+        <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
+        <q-toolbar-title >
+            <p class="q-mt-none q-mb-none" style="font-size:small">Script Explorer</p>
+            
+          
         </q-toolbar-title>
 
-        <div>Quasar v{{ $q.version }}</div>
+        <q-input class="q-mb-xs" v-model="pID" v-if="!loggedIn" label="Project ID:" placeholder="Project ID"  />
+        <q-btn flat round icon="login" v-if="!loggedIn" @click="store.setApi(pID); pID=''"/>
+        <q-btn no-caps flat rounded class="q-mt-none q-mb-none" style="font-size:smaller" color="accent" :label="pidDisplay" v-if="loggedIn" @click="store.clearApi"/>
+
+        <q-btn dense flat round icon="menu" @click="toggleRightDrawer" />
       </q-toolbar>
     </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
-      <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
+    <q-drawer show-if-above v-model="leftDrawerOpen" side="left" bordered>
+      <LeftList />
+    </q-drawer>
 
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
+    <q-drawer overlay show-if-above v-model="rightDrawerOpen" side="right" bordered>
+      <RightList />
     </q-drawer>
 
     <q-page-container>
       <router-view />
     </q-page-container>
+
   </q-layout>
 </template>
 
-<script>
-import { defineComponent, ref } from 'vue'
-import EssentialLink from 'components/EssentialLink.vue'
+<script setup>
+import { ref, computed } from 'vue'
+import LeftList from './LeftList.vue';
+import RightList from './RightList.vue';
+import { useScriptStore } from '../stores/script-store'
 
-const linksList = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-]
+const store = useScriptStore();
 
-export default defineComponent({
-  name: 'MainLayout',
+const pID = ref('')
 
-  components: {
-    EssentialLink
-  },
+const leftDrawerOpen = ref(false)
+const rightDrawerOpen = ref(false)
 
-  setup () {
-    const leftDrawerOpen = ref(false)
-
-    return {
-      essentialLinks: linksList,
-      leftDrawerOpen,
-      toggleLeftDrawer () {
-        leftDrawerOpen.value = !leftDrawerOpen.value
-      }
-    }
-  }
+const loggedIn = computed(() => {
+  return store.ApiDetails.pid !== ''
 })
+const pidDisplay = computed(() => {
+  return store.ApiDetails.pid.slice(0, 7) + " - " + store.ApiDetails.pid.slice(7,14)+ "..."+ store.ApiDetails.pid.slice(store.ApiDetails.pid.length-7)
+})
+
+const toggleLeftDrawer = () => {
+  leftDrawerOpen.value = !leftDrawerOpen.value
+}
+const toggleRightDrawer = () => {
+  rightDrawerOpen.value = !rightDrawerOpen.value
+}
+
 </script>
--- scr-exp-app/src/pages/IndexPage.vue
+++ scr-exp-app/src/pages/IndexPage.vue
@@ -1,17 +1,209 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page>
+
+    <div class="q-pa-md">
+      <q-input text spellcheck="off" bottom-slots v-model="searchScript" label="Find a Script">
+        <template v-slot:append>
+          <q-icon name="search" @click="store.CurScript = searchScript; store.loadScriptData()" />
+        </template>
+        <template v-slot:before>
+          <q-icon v-if="searchScript !== ''" name="close" @click="searchScript = ''" class="cursor-pointer" />
+        </template>
+      </q-input>
+    </div>
+
+<div class="row" v-if="store.CurScript == ''">
+  <h2 class="col-7 col-sm-4 text-right q-mt-none q-mb-none">Script</h2>
+  <div class="col-auto popover__title popover__wrapper" v-if="store.CurScript == ''">      
+      <h2 class="text-center q-mt-none q-mb-none">-</h2>
+           
+      <p class="q-mt-xl q-mb-none popover__content" style="font-size:10px;">by Quirin_Schlegel</p>
+    </div>
+    <p class="col-5 col-sm-auto"></p> 
+    <h2 class="col-4 text-left q-mt-none q-mb-none">Explorer</h2>
+</div>
+
+    
+
+
+    <q-card bordered class="q-ma-md" v-if="store.CurScript != ''">
+      <q-card-section class="row">
+        <div class="text-h5 q-mt-sm q-mb-xs col text-center" style="word-wrap:break-word">
+          📜 {{ store.CurScript }}
+          <q-btn flat round icon="content_copy" @click='cTC' />
+
+        </div>
+
+      </q-card-section>
+
+
+
+      <q-card-section v-if='variant == "timelock"'>
+        <q-separator />
+        <q-list bordered padding class="rounded-borders">
+
+          <q-item>
+            <q-item-section avatar>
+              <q-icon name="lock_outline" />
+            </q-item-section>
+            <q-item-section>
+              {{ store.ScrJson.type }}
+            </q-item-section>
+
+            <q-item-section v-if='store.ScrJson.type == "atLeast"'>
+              {{ store.ScrJson.required }}
+            </q-item-section>
+          </q-item>
+
+          <div v-if='store.ScrJson.type == "sig"'>
+            <q-separator spaced />
+            <q-item v-if='store.ScrJson.type == "sig"'>
+              {{ store.ScrJson.keyHash }}
+              <q-icon name="key" />
+            </q-item>
+          </div>
+
+          <div v-if='store.ScrJson.type != "sig"'>
+            <div v-for="s, index in store.ScrJson.scripts" :key="index">
+
+              <q-separator spaced />
+
+              <q-item v-if='s.type == "sig"'>
+                {{ s.keyHash }}
+                <q-icon name="key" />
+              </q-item>
+
+              <q-item v-if='s.type == "before"'>
+                <q-icon name="last_page" /> {{ s.slot }}
+                <q-icon name="schedule" />
+              </q-item>
+
+              <q-item v-if='s.type == "after"'>
+                {{ s.slot }}
+                <q-icon name="schedule" />
+                <q-icon name="first_page" />
+              </q-item>
+            </div>
+          </div>
+        </q-list>
+      </q-card-section>
+
+      <RedeemerView v-if='variant == "plutus" && rLoaded' />
+      <q-linear-progress query track-color="primary" color="accent" class="q-mt-sm"
+        v-if='variant == "plutus" && !rLoaded' />
+    </q-card>
   </q-page>
 </template>
 
-<script>
-import { defineComponent } from 'vue'
+<script setup>
+import { computed, ref } from 'vue';
+import { useScriptStore } from 'src/stores/script-store';
+import RedeemerView from 'src/components/RedeemerView.vue';
 
-export default defineComponent({
-  name: 'IndexPage'
+const store = useScriptStore();
+
+const searchScript = ref('')
+
+const cTC = function () {
+  const textToCopy = store.CurScript
+  if (navigator.clipboard && window.isSecureContext) {
+    // navigator clipboard api method'
+    return navigator.clipboard.writeText(textToCopy);
+  } else {
+    // text area method
+    let textArea = document.createElement("textarea");
+    textArea.value = textToCopy;
+    // make the textarea out of viewport
+    textArea.style.position = "fixed";
+    textArea.style.left = "-999999px";
+    textArea.style.top = "-999999px";
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+    //return new Promise((res, rej) => {
+      // here the magic happens
+    //  document.execCommand('copy') ? res() : rej();
+     // textArea.remove();
+    //});
+  }
+}
+
+const rLoaded = computed(() => {
+  const rAvailable = store.ScrRList.filter((f) => f.script_hash === store.CurScript).length > 0
+  if (!rAvailable) store.loadScriptRedemer()
+  return rAvailable
 })
+
+const variant = computed(() => {
+  const x = store.ScrDataList.filter((f) => f.script_hash === store.CurScript);
+  if (x[0] !== undefined) {
+    switch (x[0].type) {
+      case "timelock": store.loadScriptJson(); return "timelock";
+      case "plutus": store.loadScriptRedemer(); return "plutus";
+      case "plutusV1": store.loadScriptRedemer(); return "plutus";
+      case "plutusV2": store.loadScriptRedemer(); return "plutus";
+      default: return "err";
+    }
+  } return "err"
+}
+)
+
 </script>
+
+
+<style>
+.popover__wrapper {
+    position: relative;
+}
+
+.popover__content {
+    opacity: 0;
+    visibility: hidden;
+    position: absolute;
+    font-size: 12px;
+    left: 0%;
+    top: 90%;
+    transform: translate(0px, 10px);
+    border-radius: 5px;
+    color: black;
+    background-color: white;
+    padding: 0.5em;
+    border: 1px solid #dc500f;
+    box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.26);
+
+}
+
+.popover__content::after,
+.popover__content::before {
+    display: block;
+    box-sizing: border-box;
+    border-right: 1px solid transparent;
+    background-color: white;
+
+}
+
+.popover__content::before {
+    content: "";
+    position: absolute;
+    width: 12px;
+    height: 12px;
+    border-left: 1px solid #dc500f;
+    border-top: 1px solid #dc500f;
+    border-bottom: 1px solid transparent;
+
+    transform: rotate(45deg) skew(10deg, 10deg);
+    top: -6px;
+    left: 8px;
+    bottom: -13px;
+    transition-duration: 0.3s;
+    transition-property: transform;
+}
+
+.popover__wrapper:hover .popover__content {
+    z-index: 10;
+    opacity: 1;
+    visibility: visible;
+    transform: translate(0px, 0px);
+    transition: all 0.5s cubic-bezier(0.75, -0.02, 0.2, 0.97);
+}
+</style>
